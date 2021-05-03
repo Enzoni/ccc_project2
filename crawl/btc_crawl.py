52da181ecf3f66403579d2facee3b4fd244cdd8e
vesper <enzonii@outlook.com>
2021-05-03 22:23:47 +0800

    Fix: crawl exception doesn't work

--- crawl/btc_crawl.py
+++ crawl/btc_crawl.py
@@ -129,4 +129,4 @@ if __name__ == '__main__':
         except Exception as e:
             print(e)
             time.sleep(10)
-            main(args)
+
--- crawl/harvester.sh
+++ crawl/harvester.sh
@@ -5,9 +5,9 @@ pip3 install -r config/requirements.txt
 
 echo "Running harvester"
 
-nohup python3 btc_crawl.py -a=8 -c=Sydney     -d=btc_1 >> output_s.txt 2>&1 &
-nohup python3 btc_crawl.py -a=8 -c=Melbourne  -d=btc_2 >> output_m.txt 2>&1 &
-nohup python3 btc_crawl.py -a=4 -c=Brisbane   -d=btc_3 >> output_b.txt 2>&1 &
-nohup python3 btc_crawl.py -a=4 -c=Perth      -d=btc_4 >> output_p.txt 2>&1 &
-nohup python3 btc_crawl.py -a=3 -c=Gold_Coast -d=btc_5 >> output_g.txt 2>&1 &
-nohup python3 btc_crawl.py -a=3 -c=Newcastle  -d=btc_6 >> output_n.txt 2>&1 &
+nohup python3 btc_crawl.py -a=8 -c=Sydney     -d=btc_s_0504 >> output_s.txt 2>&1 &
+nohup python3 btc_crawl.py -a=8 -c=Melbourne  -d=btc_m_0504 >> output_m.txt 2>&1 &
+nohup python3 btc_crawl.py -a=4 -c=Brisbane   -d=btc_b_0504 >> output_b.txt 2>&1 &
+nohup python3 btc_crawl.py -a=4 -c=Perth      -d=btc_p_0504 >> output_p.txt 2>&1 &
+nohup python3 btc_crawl.py -a=3 -c=Gold_Coast -d=btc_g_0504 >> output_g.txt 2>&1 &
+nohup python3 btc_crawl.py -a=3 -c=Newcastle  -d=btc_n_0504 >> output_n.txt 2>&1 &
