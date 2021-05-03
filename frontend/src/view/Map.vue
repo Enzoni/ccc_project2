dea1dc86a3fdb1dfd24686c1d7c3643404174eb5
ziyliu2 <48190437+ziyliu2@users.noreply.github.com>
2021-05-04 00:11:12 +1000

    1
    frontend

---
+++ frontend/src/view/Map.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+  <MapVisual2></MapVisual2>
+  </div>
+</template>
+
+<script>
+import MapVisual2 from '@/components/MapComponents/MapVisual2'
+export default {
+  components :{
+    MapVisual2,
+  }
+}
+</script>
