fe27df768fd9c86c23a2e6e734bdb7e6a13f6c9a
gokying <80245526+gokying@users.noreply.github.com>
2021-05-24 15:55:18 +1000

    Update setup_db.py

--- couchdb/setup_db.py
+++ couchdb/setup_db.py
@@ -1,3 +1,14 @@
+# COMP90024 Assignment 2
+# Team: 38
+# City: Melbourne
+# Members:
+# Ziran Gu (1038782)
+# Jueying Wang (1016724)
+# Yifei Zhou(980429)
+# Jiakai Ni (988303)
+# Ziyue Liu (1036109)
+
+
 import couchdb
 from couchdb import design
 from datetime import datetime
