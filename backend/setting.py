f71283fcd77569f7f3be290446aadc22c6ef7edd
yifeizhou980429 <82931242+yifeizhou980429@users.noreply.github.com>
2021-05-25 14:41:34 +0800

    Update setting.py

--- backend/setting.py
+++ backend/setting.py
@@ -1,3 +1,13 @@
+# COMP90024 Assignment 2
+# Team: 38
+# City: Melbourne
+# Members:
+# Ziran Gu (1038782)
+# Jueying Wang (1016724)
+# Yifei Zhou(980429)
+# Jiakai Ni (988303)
+# Ziyue Liu (1036109)
+
 # coding: utf-8
 host = "172.26.132.206"
 port = "5984"
