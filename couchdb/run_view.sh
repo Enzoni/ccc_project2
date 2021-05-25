ee9c56d4e449fc1d461c260a03b2c82a2fd48678
gokying <80245526+gokying@users.noreply.github.com>
2021-05-25 18:08:37 +1000

    Update run_view.sh

--- couchdb/run_view.sh
+++ couchdb/run_view.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# COMP90024 Assignment 2
+# Team: 38
+# City: Melbourne
+# Members:
+# Ziran Gu (1038782)
+# Jueying Wang (1016724)
+# Yifei Zhou(980429)
+# Jiakai Ni (988303)
+# Ziyue Liu (1036109)
+
+
 pip3 install couchdb
 
 nohup python3 setup_db.py >> output.txt 2>&1
